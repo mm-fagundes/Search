0579e37aa062ebc2fbab62c3b483b9f3fa41bca9
Mateus Fagundes <mmsf.fagundes@gmail.com>
2025-08-27 09:09:07 -0300

    Diversas Modificações
    Adições dos cartões, do titulo principal, e e inicio da estilização deles.

--- pags/index.php
+++ pags/index.php
@@ -5,8 +5,126 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
+    <?php include 'links.php'?>
 </head>
 <body>
     
+<!-- Navbar -->
+<?php include 'navbar.php'?>
+
+<div class="master">
+
+    <div class="part1">
+        <div class="div1">
+            <h1 id="tt1">Buscar é fácil, encontrar o que te atende é Search.</h1>
+        </div>
+            <div class="menu">
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+                <div class="cartao">
+                    <div class="tt-cartao">
+                        <h1>Padeiro Roberto</h1>
+                    </div>
+                    <div class="img-container">
+                        <img src="../imgs/padeiro.jpeg" alt="" width="200px">
+                    </div>
+                    
+                </div>
+            </div>
+        </div>    
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
